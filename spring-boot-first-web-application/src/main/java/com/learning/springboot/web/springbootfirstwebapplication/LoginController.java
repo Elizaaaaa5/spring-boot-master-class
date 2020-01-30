30c70dae255ecb1b1e0dbb54bc4d0b7c6234aa60
Elizaaaaa5 <luyao9974@gmail.com>
2020-01-29 23:34:10 -0500

    creating jsp page.

---
+++ spring-boot-first-web-application/src/main/java/com/learning/springboot/web/springbootfirstwebapplication/LoginController.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.web.springbootfirstwebapplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String loginMessage(){
+        return "login";
+    }
+}
